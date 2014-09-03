fc01724b0b64ac975e0dc4b254997d8a2451445d
Takeshi YAEDA <t.yaeda@gmail.com>
2014-09-03 22:19:47 +0900

    Add join message test and initial implementation

--- test/api.js
+++ test/api.js
@@ -15,10 +15,62 @@ describe('DF Server', function () {
     helper.startServer(done);
   });
 
+  after(function() {
+    helper.stopServer();
+  });
+
   it('should response when connected', function (done) {
 
     var client = io.connect(socketURL, options);
-    client.on('connect', done);
+    client.on('connect', function () {
+      client.disconnect();
+      done();
+    });
+
+  });
+
+  describe('join message', function () {
+
+    var client = null;
+
+    beforeEach(function (done) {
+      client = io.connect(socketURL, options);
+      client.on('connect', done);
+    });
+
+    afterEach(function () {
+      client.disconnect();
+    });
+
+    it('should returns error when argument is nothing', function (done) {
+
+      client.emit('join');
+      client.on('joined', function (response) {
+        expect(response.error).to.be.ok();
+        done();
+      });
+
+    });
+
+    it('should returns error when argument is only name', function (done) {
+
+      client.emit('join', {name: 'foo'});
+      client.on('joined', function (response) {
+        expect(response.error).to.be.ok();
+        done();
+      });
+
+    });
+
+    it('should returns without error when name and role are passed', function (done) {
+
+      client.emit('join', {name: 'foo', role: 't'});
+      client.on('joined', function (response) {
+        expect(response.error).to.not.be.ok();
+        done();
+      });
+
+    });
 
   });
 
