edd65b045de1eece2ee6e2e3807aee8222a5ae01
Takeshi YAEDA <t.yaeda@gmail.com>
2014-09-12 17:35:16 +0200

    :green_heart: Add accept-leave test

--- test/leave.js
+++ test/leave.js
@@ -65,4 +65,74 @@ describe('Scenario 2 :', function () {
 
   });
 
+  describe('leave after mached', function () {
+
+    var client1 = null;
+    var client2 = null;
+    var client3 = null;
+    var client4 = null;
+
+    beforeEach(function (done) {
+      client1 = io.connect(socketURL, options);
+      client2 = io.connect(socketURL, options);
+      client3 = io.connect(socketURL, options);
+      client4 = io.connect(socketURL, options);
+      client1.on('connect', function () { client1.emit('join', {name: 'foo', role: 't'}); });
+      client2.on('connect', function () { client2.emit('join', {name: 'bar', role: 'd'}); });
+      client3.on('connect', function () { client3.emit('join', {name: 'fiz', role: 'd'}); });
+      client4.on('connect', function () { client4.emit('join', {name: 'buz', role: 'h'}); });
+      client1.on('matched', done);
+    });
+
+    afterEach(function () {
+      client1.disconnect();
+      client2.disconnect();
+      client3.disconnect();
+      client4.disconnect();
+    });
+
+    it('shoule be emit `canceled` event to all player', function (done) {
+      var called = 0;
+      var callback = function () {
+        verifyHelper.verifyParties(1);
+        called++;
+        if (called === 4) {
+          done();
+        }
+      };
+
+      client1.emit('accept');
+      client2.emit('accept');
+      client3.emit('accept');
+      client4.emit('leave');
+      client1.on('canceled', callback);
+      client2.on('canceled', callback);
+      client3.on('canceled', callback);
+      client4.on('canceled', callback);
+    });
+
+    it('should remove only leaved player from the party', function (done) {
+      var called = 0;
+      var callback = function () {
+        called++;
+        if (called === 4) {
+          done();
+        }
+      };
+
+      client1.emit('accept');
+      client2.emit('accept');
+      client3.emit('accept');
+      client4.emit('leave');
+      client4.on('canceled', function () {
+        client4.emit('join', {name: 'buz', role: 'h'});
+      });
+      client1.on('matched', callback);
+      client2.on('matched', callback);
+      client3.on('matched', callback);
+      client4.on('matched', callback);
+    });
+
+  });
+
 });
